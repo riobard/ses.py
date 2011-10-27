768dc8e5961ed32d388588146914bc0d89c3b773
Riobard <me@riobard.com>
2011-10-27 22:17:51 +0800

    Revamped the error response hierarchy

--- ses.py
+++ ses.py
@@ -8,16 +8,53 @@ from getopt import gnu_getopt as getopt, GetoptError
 
 
 class SESError(Exception):
-    def __init__(self, msg=None, **kargs):
+    ''' Base error. All SES-related exceptions are dervied from this one. '''
+    def __init__(self, msg=None, rsp=None):
         self.msg    = msg
-        self.kargs  = kargs
+        self.rsp    = rsp
 
     def __str__(self):
-        s = [] if self.msg is None else [self.msg]
-        for (k, v) in self.kargs.items():
-            s.append('{0}={1}'.format(k, v))
-        rs = '|'.join(s)
-        return rs
+        return '{msg} | {status} {reason} {headers} {body}'.format(
+                msg = self.msg, 
+                status = self.rsp.status,
+                reason = self.rsp.reason, 
+                headers = self.rsp.msg,
+                body = self.rsp.body)
+
+
+
+## These are the exceptions that are most encountered. 
+class SESThrottling(SESError): 
+    ''' SES is throttling the sending process
+    
+    Usually this is due to exceeding max send rate or daily quota'''
+    pass
+
+class SESMaxSendRateExceeded(SESThrottling): 
+    ''' Max send rate is exceeded. Slow down. '''
+    pass
+
+class SESDailyQuotaExceeded(SESThrottling):
+    ''' Daily quota is exceeded. Stop sending until quota is replenished. '''
+    pass
+
+
+class SESMessageRejected(SESError): 
+    ''' SES rejects the message for sending. 
+
+    Read the error message to see why. 
+    '''
+    pass
+
+class SESAddressBlacklisted(SESMessageRejected):
+    ''' SES rejects the message for sending because the address is blacklisted. 
+
+    An email address is blacklisted if SES received too many bounces or 
+    complaints from the receipient's ISP. An email will stay in the blacklist
+    for a while. Do not send to this address unless it gets delisted. 
+    '''
+    pass
+
 
 
 def extract_xml(xml, keys, multiple=False):
@@ -31,34 +68,55 @@ def extract_xml(xml, keys, multiple=False):
                 rs[key] = [e.childNodes[0].nodeValue for e in ls]
             else:
                 rs[key] = ls[0].childNodes[0].nodeValue
-
         return rs
     
     except IndexError as e:
-        raise SESError('Failed to extract values from XML: {0}'.format(xml))
+        raise SESError('Failed to extract keys {keys} from XML: {0}'.format(keys, xml))
 
 
 
-class SES(object):
+def decode_error_response(xml):
+    dom = minidom.parseString(xml)
+    if len(dom.getElementsByTagName('ErrorResponse')) > 0:
+        try:
+            rs = extract_xml(xml, ['Type', 'Code', 'Message'])
+            errtype = rs['Type']
+            errcode = rs['Code']
+            errmsg  = rs['Message']
+            return errtype, errcode, errmsg
+        except:
+            return None
+    else:
+        return None
+
+
+
+class SESConnection(object):
+    '''
+    A persistent connection to SES endpoint
+
+        This is NOT THREAD-SAFE! We are reusing a single HTTPS connection to 
+        do multiple request/reply cycles. 
+    '''
 
     API_VERSION = '2010-12-01'
+    API_XML_NAMESPACE = 'http://ses.amazonaws.com/doc/{date}/'.format(date=API_VERSION)
     API_ENDPOINT = 'email.us-east-1.amazonaws.com'  # just the host
-    API_REQUEST_TIMEOUT = 30    # seconds
+    API_REQUEST_TIMEOUT = 15    # seconds
 
     def __init__(self, key_id, key, api_endpoint = None):
         self.key_id = key_id
         self.key    = key
         if api_endpoint is not None:
             self.API_ENDPOINT = api_endpoint
+
+        # The persistent HTTPS connection object
         self.conn = httplib.HTTPSConnection(host=self.API_ENDPOINT,
-                                             timeout=self.API_REQUEST_TIMEOUT)
+                                            timeout=self.API_REQUEST_TIMEOUT)
 
 
     def api(self, body):
-        ''' Call AWS SES service API 
-        
-        This is NOT THREAD-SAFE!
-        '''
+        ''' Call AWS SES service API '''
         # RFC2822 date format
         date = datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S +0000')
         signature = b64encode(hmac.new(self.key, date, sha256).digest())
@@ -74,23 +132,64 @@ class SES(object):
         try:
             self.conn.request('POST', '/', headers=headers, body=post_data)
             rsp = self.conn.getresponse()
-            if rsp.status == 200:
-                return rsp.read()
-            else:
-                msg = 'HTTP request error: HTTP {version} {status} {reason}\n'\
-                      '{headers}\n\n{body}'.format(
-                              version=rsp.version, 
-                              status=rsp.status,
-                              reason=rsp.reason,
-                              headers=rsp.msg,
-                              body=rsp.read())
-                raise SESError(msg.format(str(rsp)))
-
+            return self.process_response(rsp)
         except httplib.HTTPException as e:
-            raise SESError(str(e))
-
+            try: 
+                raise SESError(str(e), rsp=rsp)
+            except NameError:
+                raise SESError(str(e))
         except IOError as e:
-            raise SESError(str(e))
+            try: 
+                raise SESError(str(e), rsp=rsp)
+            except NameError:
+                raise SESError(str(e))
+
+
+    def process_response(self, rsp):
+        ''' Process respsonse from SES endpoint '''
+
+        try:
+            rsp.content_type = rsp.msg['content-type'].lower()
+        except KeyError:
+            rsp.content_type = ''
+
+        try:
+            rsp.content_length = int(rsp.msg['content-length'])
+        except (ValueError, KeyError):
+            rsp.content_length = -1
+
+        rsp.body = rsp.read() if rsp.content_length > 0 else ''
+
+        if rsp.content_type != 'text/xml':
+            raise SESError('Response content-type is not text/xml', rsp=rsp)
+
+        if len(rsp.body) != rsp.content_length:
+            raise SESError('Incomplete response', rsp=rsp)
+
+        if rsp.status == 200:
+            return rsp.body
+        else:
+            rs = decode_error_response(rsp.body)
+            if rs is not None:
+                errtype, errcode, errmsg = rs
+                msg = errmsg.lower()
+                
+                if errcode == 'MessageRejected':
+                    if 'address' in msg and 'blacklisted' in msg:
+                        raise SESAddressBlacklisted(errmsg, rsp=rsp)
+                    else:
+                        raise SESMessageRejected(errmsg, rsp=rsp)
+                elif errcode == 'Throttling':
+                    if 'rate' in msg:
+                        raise SESMaxSendRateExceeded(errmsg, rsp=rsp)
+                    elif 'quota' in msg:
+                        raise SESDailyQuotaExceeded(errmsg, rsp=rsp)
+                    else:
+                        raise SESThrottling(errmsg, rsp=rsp)
+                else:
+                    raise SESError(errmsg, rsp=rsp)
+            else:
+                raise SESError('Unexpected response', rsp=rsp)
 
 
     @property
@@ -353,8 +452,8 @@ Credentials file example
 
         try:
             key_id, key = get_credentials(opts)
-            ses         = SES(key_id, key)
-            getattr(Actions, action)(ses, opts, args)
+            sesconn = SESConnection(key_id, key)
+            getattr(Actions, action)(sesconn, opts, args)
         except SESError as e:
             sys.exit('Error: {0}'.format(e))
     
